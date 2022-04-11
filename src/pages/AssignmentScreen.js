85b34e953f10c5e08396704e5ac8dbb3d110d62c
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-04-11 15:36:11 +0530

    fixed bug

--- src/pages/AssignmentScreen.js
+++ src/pages/AssignmentScreen.js
@@ -9,17 +9,12 @@ import {
 } from 'firebase/firestore';
 import { collection, query, where } from 'firebase/firestore';
 import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
-import { db } from 'src/firebase/firebase-config';
+import { db, storage } from 'src/firebase/firebase-config';
 import CloseIcon from '@mui/icons-material/Close';
 import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
 import { getAuth } from 'firebase/auth';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { v4 as uuidv4 } from 'uuid';
+
 import {
   Typography,
   Button,
@@ -32,24 +27,121 @@ import {
   Box,
   Grid,
   Alert,
+  Input,
 } from '@mui/material';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import moment from 'moment';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
-import Announcements from 'src/components/Announcements';
-import ClassWork from 'src/components/ClassWork';
-import ClassSubmissions from 'src/components/Submissions';
-import Assignments from 'src/components/Assignments';
-import ClassStudents from 'src/components/ClassStudents';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import LinearWithValueLabel from 'src/components/LinearProgressWithLabel';
 
 const AssignmentScreen = () => {
   let { id } = useParams();
-  const [assignment, setAssignment] = useState(null);
+  const auth = getAuth();
+  const [title, settitle] = React.useState('');
 
-  console.log(id);
+  const [assignment, setAssignment] = useState(null);
+  const [open, setopen] = React.useState(false);
+  const [submissionOpen, setSubmissionOpen] = React.useState(false);
+  const [uploadingopen, setuploadingopen] = React.useState(false);
+  const [uploadingProgress, setuploadingProgress] = useState(0);
+  const [uploadingMessage, setuploadingMessage] = useState('');
+  const [user, loading, error] = useAuthState(auth);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [isFilePicked, setIsFilePicked] = useState(false);
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const changeHandler = (event) => {
+    if (event.target.files[0]) {
+      setSelectedFile(event.target.files[0]);
+      setIsFilePicked(true);
+    } else {
+      setSelectedFile(null);
+      setIsFilePicked(false);
+    }
+  };
+
+  const handleAssignmentSubmission = () => {
+    // Upload file and metadata to the object 'images/mountains.jpg'
+    const storageRef = ref(storage, 'assignments/' + selectedFile.name);
+    const uploadTask = uploadBytesResumable(storageRef, selectedFile);
+
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        setuploadingopen(true);
+        setuploadingProgress(progress);
+        switch (snapshot.state) {
+          case 'paused':
+            setuploadingMessage('Upload is paused');
+            break;
+          case 'running':
+            setuploadingMessage('Upload is running');
+            break;
+        }
+      },
+      (error) => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case 'storage/unauthorized':
+            // User doesn't have permission to access the object
+            break;
+          case 'storage/canceled':
+            // User canceled the upload
+            break;
+
+          // ...
+
+          case 'storage/unknown':
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+      },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+          // console.log('File available at', downloadURL);
+          let submissions = assignment.submissions.push({
+            submittedBy: user.uid,
+            submittedUser: user.displayName,
+            submittedAt: moment().format('h:mm a, Do MMMM YYYY'),
+            fileUrl: downloadURL,
+          });
+          const assignmentRef = doc(db, 'assignments', id);
+          await updateDoc(assignmentRef, {
+            submissions: submissions,
+          });
+        });
+
+        handleClose();
+        setSelectedFile(null);
+        setIsFilePicked(false);
+      }
+    );
+  };
+
+  const handleClose = () => {
+    setopen(false);
+  };
+  const handleOpen = () => {
+    setopen(true);
+  };
+
+  function formatBytes(bytes, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  }
 
   useEffect(async () => {
     const assignmentRef = collection(db, 'assignments');
@@ -64,6 +156,133 @@ const AssignmentScreen = () => {
 
   return (
     <Container>
+      {open && (
+        <Card
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: '50%',
+            bgcolor: '#fff',
+            paddingX: 3,
+            paddingY: 4,
+            zIndex: 100,
+          }}
+        >
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Typography gutterBottom variant='h5' component='div'>
+              Add Submission
+            </Typography>
+            <IconButton onClick={() => setopen(false)}>
+              <CloseIcon />
+            </IconButton>
+          </div>
+
+          <CardContent>
+            <div>
+              <Grid
+                container
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  mb: 3,
+                }}
+              >
+                <Grid item>
+                  <Input
+                    type='file'
+                    name='file'
+                    color='primary'
+                    disableUnderline
+                    fullWidth
+                    margin='dense'
+                    onChange={changeHandler}
+                  />
+                </Grid>
+                {isFilePicked && (
+                  <Grid item>
+                    <IconButton
+                      onClick={() => {
+                        setIsFilePicked(false);
+                        setSelectedFile(null);
+                      }}
+                    >
+                      <CloseIcon />
+                    </IconButton>
+                  </Grid>
+                )}
+              </Grid>
+
+              {isFilePicked && (
+                <div>
+                  <p>Filename: {selectedFile.name}</p>
+                  <p>Size: {formatBytes(selectedFile.size)}</p>
+                  {/* <p>
+                    lastModifiedDate:{' '}
+                    {selectedFile.lastModifiedDate.toLocaleDateString()}
+                  </p> */}
+                </div>
+              )}
+            </div>
+          </CardContent>
+
+          <CardActions>
+            <Button size='small' onClick={() => setopen(false)}>
+              Cancel
+            </Button>
+            <Button size='small' onClick={handleAssignmentSubmission}>
+              Submit
+            </Button>
+          </CardActions>
+        </Card>
+      )}
+
+      {uploadingopen && (
+        <Card
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: '50%',
+            bgcolor: '#fff',
+            paddingX: 3,
+            paddingY: 4,
+            zIndex: 10,
+          }}
+        >
+          <CardContent>
+            {uploadingProgress < 100 && (
+              <Typography gutterBottom variant='h5' component='div'>
+                {uploadingMessage}
+              </Typography>
+            )}
+
+            {uploadingProgress < 100 ? (
+              <LinearWithValueLabel progress={uploadingProgress} />
+            ) : (
+              <Typography gutterBottom variant='h6' component='div'>
+                File uploaded successfully
+              </Typography>
+            )}
+          </CardContent>
+
+          <CardActions>
+            <Button
+              disabled={uploadingProgress !== 100}
+              size='small'
+              onClick={() => {
+                setuploadingopen(false);
+                // fetchAssignments();
+                setuploadingProgress(0);
+              }}
+            >
+              Done
+            </Button>
+          </CardActions>
+        </Card>
+      )}
       {assignment ? (
         <Container>
           <Grid container spacing={2}>
@@ -138,6 +357,7 @@ const AssignmentScreen = () => {
                 <Button
                   variant='contained'
                   sx={{ boxShadow: 'none', width: '100%' }}
+                  onClick={handleOpen}
                 >
                   Add Submission
                 </Button>
